4def651091e496bb35297766428e26169360ad69
Hani (HMK) <hmkcode@gmail.com>
2013-04-16 15:41:01 +0300

    Math.java

---
+++ simple-junit/Math.java
@@ -0,0 +1,29 @@
+package com.hmkcode.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class Math 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public double sum(double x, double y){
+      return x+y;
+    }
+    public double multiply(double x, double y){
+      return x*y;
+    }
+    public double divide(double x, double y){
+      return x/y;
+    }
+    public double subtract(double x, double y){
+      return x - y;
+    }
+    
+    
+    
+}
