fe6b2e43ec4e2644c8f5db81be5e0ed9d42cbe38
Hani (HMK) <hmkcode@gmail.com>
2013-07-24 12:26:05 +0300

    java
    java

---
+++ itext-java-html-pdf/main/java/com/hmkcode/App.java
@@ -0,0 +1,33 @@
+package com.hmkcode;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
+
+public class App 
+{
+    public static void main( String[] args ) throws DocumentException, IOException
+    {
+      // step 1
+    	Document document = new Document();
+        // step 2
+    	PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("pdf.pdf"));
+        // step 3
+        document.open();
+        // step 4
+        //CSSResolver cssResolver = XMLWorkerHelper.getInstance().getDefaultCssResolver(true);
+
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                new FileInputStream("index.html"));	
+        //step 5
+         document.close();
+
+        System.out.println( "PDF Created!" );
+    }
+}
