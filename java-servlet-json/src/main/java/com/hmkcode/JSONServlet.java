0337beb7109324711b07aea1102d15fd3ec96d80
Logarifm <logarifm6@gmail.com>
2014-08-17 20:15:07 +0300

    Texts from another language(Japanese, Ukrainian and many other) had displayed wrong. This is commit fixs this problems with charset. Added new servlet method get and new javascript function getData(), which rebuid list of articles after reloading page

--- java-servlet-json/src/main/java/com/hmkcode/JSONServlet.java
+++ java-servlet-json/src/main/java/com/hmkcode/JSONServlet.java
@@ -1,17 +1,16 @@
 package com.hmkcode;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hmkcode.vo.Article;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hmkcode.vo.Article;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class JSONServlet extends HttpServlet {
 	
@@ -23,25 +22,28 @@ public class JSONServlet extends HttpServlet {
 	/***************************************************
 	 * URL: /jsonservlet
 	 * doPost(): receives JSON data, parse it, map it and send back as JSON
+     * doGet(): receives all JSON data, because after reloading page all
+     * data would clear
 	 ****************************************************/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 	        throws ServletException, IOException{
 	    
 		// 1. get received JSON data from request
-		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-		String json = "";
-		if(br != null){
-			json = br.readLine();
-		}
+        StringBuffer json = new StringBuffer();
+        String line = null;
+        BufferedReader br = new BufferedReader(request.getReader());
+        while((line = br.readLine()) != null) {
+            json.append(line);
+        }
 		
 		// 2. initiate jackson mapper
     	ObjectMapper mapper = new ObjectMapper();
     	
     	// 3. Convert received JSON to Article
-    	Article article = mapper.readValue(json, Article.class);
+    	Article article = mapper.readValue(json.toString(), Article.class);
 
 		// 4. Set response type to JSON
-		response.setContentType("application/json");		    
+		response.setContentType("application/json; charset=utf-8");
     	
     	// 5. Add article to List<Article>
 		articles.add(article);
@@ -49,4 +51,11 @@ public class JSONServlet extends HttpServlet {
 		// 6. Send List<Article> as JSON to client
     	mapper.writeValue(response.getOutputStream(), articles);
 	}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json; charset=utf-8");
+        mapper.writeValue(response.getOutputStream(), articles);
+    }
 }
--- java-servlet-json/src/main/webapp/js/myfunctions.js
+++ java-servlet-json/src/main/webapp/js/myfunctions.js
@@ -1,50 +1,59 @@
 function sendAjax() {
- 
-	// get inputs
-	var article = new Object();
-	article.title = $('#title').val();
-	article.url = $('#url').val();
-	article.categories = $('#categories').val().split(";");
-	article.tags = $('#tags').val().split(";");
-	
-	$.ajax({
-		url: "jsonservlet",
-		type: 'POST',
-		dataType: 'json',
-		data: JSON.stringify(article),
-		contentType: 'application/json',
-		mimeType: 'application/json',
-		
-		success: function (data) {
-        	$("tr:has(td)").remove();
 
-        	$.each(data, function (index, article) {
-            	
-                var td_categories = $("<td/>");
-                $.each(article.categories, function (i, tag) {
-                	var span = $("<span class='label label-info' style='margin:4px;padding:4px' />");
-                	span.text(tag);
-                	td_categories.append(span);
-                });
-                
-                var td_tags = $("<td/>");
-                $.each(article.tags, function (i, tag) {
-                	var span = $("<span class='label' style='margin:4px;padding:4px' />");
-                	span.text(tag);
-                	td_tags.append(span);
-                });
-                
-                $("#added-articles").append($('<tr/>')
-                		.append($('<td/>').html("<a href='"+article.url+"'>"+article.title+"</a>"))
-                		.append(td_categories)
-                		.append(td_tags)
-                );
-            
-                
-            }); 
+    // get inputs
+    var article = new Object();
+    article.title = $('#title').val();
+    article.url = $('#url').val();
+    article.categories = $('#categories').val().split(";");
+    article.tags = $('#tags').val().split(";");
+
+    $.ajax({
+        url: "jsonservlet",
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(article),
+        contentType: 'application/json;charset=utf-8',
+        mimeType: 'application/json;charset=utf-8',
+
+        success: function (data) {
+            drawArticles(data);
         },
-		error:function(data,status,er) {
-			alert("error: "+data+" status: "+status+" er:"+er);
-		}
-	});
+        error:function(data,status,er) {
+            alert("error: "+data+" status: "+status+" er:"+er);
+        }
+    });
+}
+
+function drawArticles(data) {
+    $("tr:has(td)").remove();
+
+    $.each(data, function (index, article) {
+
+        var td_categories = $("<td/>");
+        $.each(article.categories, function (i, tag) {
+            var span = $("<span class='label label-info' style='margin:4px;padding:4px' />");
+            span.text(tag);
+            td_categories.append(span);
+        });
+
+        var td_tags = $("<td/>");
+        $.each(article.tags, function (i, tag) {
+            var span = $("<span class='label' style='margin:4px;padding:4px' />");
+            span.text(tag);
+            td_tags.append(span);
+        });
+
+        $("#added-articles").append($('<tr/>')
+                .append($('<td/>').html("<a href='"+article.url+"'>"+article.title+"</a>"))
+                .append(td_categories)
+                .append(td_tags)
+        );
+    });
+}
+
+function getData() {
+    //get all articles after reload page
+    $.get("jsonservlet", function (data) {
+        drawArticles(data);
+    });
 }
