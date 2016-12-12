89264938ff26184aa8aae12a4f4cb79606513766
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-13 01:43:42 +0530

    github5 and github6

---
+++ xstream-java-xml/src/main/java/com/hmkcode/github5.java
@@ -0,0 +1,9 @@
+package com.hmkcode;
+
+public class github5 {
+
+	public static void main(String[] args) {
+		System.out.println("This is github5");
+	}
+
+}
---
+++ xstream-java-xml/src/main/java/com/hmkcode/github6.java
@@ -0,0 +1,9 @@
+package com.hmkcode;
+
+public class github6 {
+
+	public static void main(String[] args) {
+		System.out.println("This is github6");
+	}
+
+}
