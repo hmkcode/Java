b089b9f235dcd29a4db8b053b738c1d9b8fdd2d3
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-16 01:20:48 +0530

    New merge code

--- xstream-java-xml/src/main/java/com/hmkcode/github5.java
+++ xstream-java-xml/src/main/java/com/hmkcode/github5.java
@@ -3,7 +3,7 @@ package com.hmkcode;
 public class github5 {
 
 	public static void main(String[] args) {
-		System.out.println("This is github5");
+		System.out.println("This is my new merge code on nalinbranch2");
 	}
 
 }
