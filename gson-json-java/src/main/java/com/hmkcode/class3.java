bdb1c907c1e3ba4c69f79d1cc83824f406a9d99c
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-22 01:16:16 +0530

    Staged changes from nalinbranch1

---
+++ gson-json-java/src/main/java/com/hmkcode/class3.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is class 3");
+	}
+
+}
