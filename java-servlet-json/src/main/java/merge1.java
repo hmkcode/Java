34caed75d82af9488e11bd51ff728a3ab70fca4a
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-26 18:25:14 +0530

    merges

---
+++ java-servlet-json/src/main/java/merge1.java
@@ -0,0 +1,12 @@
+
+public class merge1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is merge1");
+		System.out.println("this is merge2");
+		System.out.println("this is merge3");
+	}
+
+}
