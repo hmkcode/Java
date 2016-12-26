9f906b1c2787a95168438a7ddd7966bdace618f7
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-26 18:42:46 +0530

    changes

---
+++ java-servlet-json/src/main/java/merge3.java
@@ -0,0 +1,13 @@
+
+public class merge3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String merge1="This is my force";
+		String merge2="This is yours";
+	
+
+	}
+
+}
