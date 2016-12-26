785bfbf6a60523c0b1efdeab9ddc5ef58c09470a
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-26 19:16:03 +0530

    change 1 and change2

---
+++ java-servlet-json/src/main/java/class4.java
@@ -0,0 +1,12 @@
+
+public class class4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String change1="This is change1";
+		String change2="This is change2";
+
+	}
+
+}
