b470725ca0a3dbe632a31ebd26ad2c4f1a166053
nalintaneja123 <nalintaneja123@gmail.com>
2016-12-26 18:32:02 +0530

    updated changes

--- java-servlet-json/src/main/java/merge1.java
+++ java-servlet-json/src/main/java/merge1.java
@@ -4,9 +4,13 @@ public class merge1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is merge1");
-		System.out.println("this is merge2");
-		System.out.println("this is merge3");
+		int a=10,b=20,c=30,d=40;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
 	}
 
 }
