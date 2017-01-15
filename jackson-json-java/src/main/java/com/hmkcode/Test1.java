66f03be4473a12e388a97470c1d78b3b631d04b1
nalintaneja123 <nalintaneja123@gmail.com>
2017-01-16 00:05:45 +0530

    Committting new test file

---
+++ jackson-json-java/src/main/java/com/hmkcode/Test1.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my Test1");
+
+	}
+
+}
