14b4690089fd6481a1964e8f46bb2509bea244fa
nalintaneja123 <nalintaneja123@gmail.com>
2017-01-16 00:10:49 +0530

    updating test1

--- jackson-json-java/src/main/java/com/hmkcode/Test1.java
+++ jackson-json-java/src/main/java/com/hmkcode/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a=24;
+		System.out.println("This is"+a);
 		System.out.println("This is my Test1");
 
 	}
