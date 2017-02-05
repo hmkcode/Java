a75011aab17fa3fd3d628a9d07350a57558f3e32
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-05 21:23:21 +0530

    pushingchanges

---
+++ java-list-map/src/main/java/com/hmkcode/testclass22.java
@@ -0,0 +1,10 @@
+package com.hmkcode;
+
+public class testclass22 {
+
+	public static void main(String[] args) {
+	System.out.println("i am putting changes from another branch on 5th february 2017");
+	
+	}
+
+}
