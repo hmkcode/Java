97a3a21b3764be3d1dfe3a34e724b2a22f26b8d7
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-10 22:57:06 +0530

    sending PR

---
+++ xstream-java-xml/src/main/java/com/hmkcode/johnclass.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class johnclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sending PR to my class");
+	}
+
+}
