0d70092081aaafbb754dd55ecaf252aa09ff883f
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-10 22:55:38 +0530

    comitting changes

---
+++ xstream-java-xml/src/main/java/com/hmkcode/SpanClass.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class SpanClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my span class");
+	}
+
+}
