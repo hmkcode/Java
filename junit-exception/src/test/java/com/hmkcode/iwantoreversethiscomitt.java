9967e20012d7f194bad943d364d13a51febe1dc1
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 21:32:09 +0530

    updating comitts

--- junit-exception/src/test/java/com/hmkcode/iwantoreversethiscomitt.java
+++ junit-exception/src/test/java/com/hmkcode/iwantoreversethiscomitt.java
@@ -5,6 +5,8 @@ public class iwantoreversethiscomitt {
 	public static void main(String[] args) {
 
 		System.out.println("This is test comitt");
+		int b=20;
+		System.out.println("This is"+b);
 
 	}
 
