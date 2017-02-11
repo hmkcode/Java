d4e80ac0cee0ea166c001c2d7b76929db9e2be3f
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 14:36:52 +0530

    sending pr changes

---
+++ junit-exception/src/test/java/com/hmkcode/prchanges.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class prchanges {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("sending u pr changes");
+	}
+
+}
