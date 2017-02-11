0c0d681fb392d1abc0871dcf7c82845b77cf456e
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 22:09:08 +0530

    changes

---
+++ junit-exception/src/test/java/com/hmkcode/testsampleclass.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class testsampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test Sample Class");
+	}
+
+}
