daa1829bf9d71d5be9e07b805e8d19b6dfcacd86
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 14:57:42 +0530

    updatedchanges

---
+++ junit-exception/src/test/java/com/hmkcode/tyclass.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class tyclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This IS PR outstanding");
+
+	}
+
+}
