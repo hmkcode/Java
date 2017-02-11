f904d897add5386c949fd4dbd1de2cc5110d00a1
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 21:48:38 +0530

    johnclass

---
+++ junit-exception/src/test/java/com/hmkcode/JohnClass.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class JohnClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is john class");
+	}
+
+}
