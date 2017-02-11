663f412fb1a7520712c14b815c15f48c9a6187e5
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 21:58:52 +0530

    changes

--- junit-exception/src/test/java/com/hmkcode/JohnClass.java
+++ junit-exception/src/test/java/com/hmkcode/JohnClass.java
@@ -9,12 +9,22 @@ public class JohnClass {
 
 		int a=30;
 		System.out.println("This is my class"+a);
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 		
 		int b=30;
 		int c=a+b;
 		System.out.println("this is mine"+c);
+<<<<<<< Updated upstream
 
+=======
+		
+		int d=50;
+		
+		System.out.println(d);
+>>>>>>> Stashed changes
 
 	}
 
