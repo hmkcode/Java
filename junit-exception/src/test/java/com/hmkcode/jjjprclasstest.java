53fd9f5282e0d14d4263659e86cac84869367c3c
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 14:53:42 +0530

    updating changes

---
+++ junit-exception/src/test/java/com/hmkcode/jjjprclasstest.java
@@ -0,0 +1,11 @@
+package com.hmkcode;
+
+public class jjjprclasstest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is PR class");
+
+	}
+
+}
