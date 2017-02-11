d81fa837318c14a4f27ccaea6cbeeec54f2394ac
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 21:35:42 +0530

    updated again

--- junit-exception/src/test/java/com/hmkcode/ThisismyLastcomitt.java
+++ junit-exception/src/test/java/com/hmkcode/ThisismyLastcomitt.java
@@ -5,6 +5,8 @@ public class ThisismyLastcomitt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my last comitt");
+		int c=20;
+		System.out.println("This is final"+c);
 
 	}
 
