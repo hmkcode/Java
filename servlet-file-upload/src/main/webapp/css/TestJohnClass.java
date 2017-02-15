d94c1519114e8171f56ee7b98aaf6095aa83df1a
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-15 21:48:43 +0530

    eer

--- servlet-file-upload/src/main/webapp/css/TestJohnClass.java
+++ servlet-file-upload/src/main/webapp/css/TestJohnClass.java
@@ -3,13 +3,10 @@ public class TestJohnClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("Hello SIR");
-=======
 
 			System.out.println("This is john travolta and dude u r doing well");
 
->>>>>>> 9fb31cfea079916920821ca8b31d22f75ce06d1f
+
 
 	}
 
