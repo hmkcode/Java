21c95a986f7b111d2dfd4e7240881222879ee69a
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-15 21:45:56 +0530

    I am updating branch

---
+++ simple-junit/src/main/java/com/hmkcode/junit/helloiui.java
@@ -0,0 +1,11 @@
+package com.hmkcode.junit;
+
+public class helloiui {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello UI");
+	}
+
+}
