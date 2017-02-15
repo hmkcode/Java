970c5c1dc482aa6ccd8f009c9344fb5dfe26a43e
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-15 22:15:35 +0530

    sending pr

---
+++ java-excel-poi/src/main/java/com/hmkcode/poi/GitProblemsClass.java
@@ -0,0 +1,11 @@
+package com.hmkcode.poi;
+
+public class GitProblemsClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my git problems class");
+	}
+
+}
