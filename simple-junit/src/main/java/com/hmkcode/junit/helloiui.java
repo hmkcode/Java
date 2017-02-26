64e27a56b73ced8757c8622d04b09e97152a62fa
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-26 20:40:18 +0530

    rrtrt

--- simple-junit/src/main/java/com/hmkcode/junit/helloiui.java
+++ simple-junit/src/main/java/com/hmkcode/junit/helloiui.java
@@ -5,10 +5,14 @@ public class helloiui {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+<<<<<<< HEAD
 
 		System.out.println("Hello UI");
 		System.out.println("Hello UI");
 
+=======
+		System.out.println("Hello UI");
+>>>>>>> I am updating branch
 	}
 
 }
