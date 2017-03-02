d4f45ad0749f306b5ae32e2d1d8c1cd1bd7245de
nalintaneja123 <nalintaneja123@gmail.com>
2017-03-02 21:16:53 +0530

    dddfd
    dd

--- xstream-java-xml/src/main/java/com/hmkcode/SpanClass.java
+++ xstream-java-xml/src/main/java/com/hmkcode/SpanClass.java
@@ -5,7 +5,7 @@ public class SpanClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is my span class");
+		System.out.println("This is my spa");
 	}
 
 }
