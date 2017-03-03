71938ac30e519e5c02880dd42309d5cf1da66b2f
nalintaneja123 <nalintaneja123@gmail.com>
2017-03-04 01:38:26 +0530

    Comit
    Git comit

---
+++ mybatis-insert-generatedkey/src/test/java/com/hmkcode/spring/mybatis/AppTest2.java
@@ -0,0 +1,20 @@
+package com.hmkcode.spring.mybatis;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;;
+
+/**
+ * Unit test for simple App.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/spring-config.xml")
+public class AppTest2 {
+
+	public static void Main(String[] args)
+	{
+		System.out.println("gg");
+		
+	}
+}
