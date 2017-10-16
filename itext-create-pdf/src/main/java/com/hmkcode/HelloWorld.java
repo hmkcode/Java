d9e7dc4a87af4357cd9f39c8214c38ac9535e056
Tatchai Russameroj <tum010@hotmail.com>
2017-10-17 02:42:22 +0700

    Formatted codes

--- itext-create-pdf/src/main/java/com/hmkcode/HelloWorld.java
+++ itext-create-pdf/src/main/java/com/hmkcode/HelloWorld.java
@@ -8,30 +8,27 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
+public class HelloWorld {
+	public static void main(String[] args) {
+		try {
 
-public class HelloWorld 
-{
-    public static void main( String[] args )
-    {
-        try {
-        	
-        	// 1. create the document
-        	Document document = new Document();
-        	// 2. get PdfWriter
+			// 1. create the document
+			Document document = new Document();
+			// 2. get PdfWriter
 			PdfWriter.getInstance(document, new FileOutputStream("helloworld.pdf"));
 			// 3. open the document
-	        document.open();
-	        // 4. add the content
-	        document.add(new Paragraph("Hello World!"));
-	        // 5. close the document
-	        document.close();
-	        
-	        System.out.println("Document created!");
-			
+			document.open();
+			// 4. add the content
+			document.add(new Paragraph("Hello World!"));
+			// 5. close the document
+			document.close();
+
+			System.out.println("Document created!");
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (DocumentException e) {
 			e.printStackTrace();
-		}      
-    }
+		}
+	}
 }
