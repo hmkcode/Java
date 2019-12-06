9224b6e55d7089dc632ff61262045cf8ca06399d
Hani M. K <hmkcode@gmail.com>
2019-12-06 21:40:20 +0300

    updated iText to version 7.1.9
    updated iText to version 7.1.9

--- itext-java-html-pdf/src/main/java/com/hmkcode/App.java
+++ itext-java-html-pdf/src/main/java/com/hmkcode/App.java
@@ -1,29 +1,31 @@
 package com.hmkcode;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import com.itextpdf.html2pdf.HtmlConverter;
 
 public class App 
 {
-    public static void main( String[] args ) throws DocumentException, IOException
+	public static final String HTML = "<h1>Hello</h1>"
+			+ "<p>This was created using iText</p>"
+			+ "<a href='hmkcode.com'>hmkcode.com</a>";
+	
+	
+    public static void main( String[] args ) throws FileNotFoundException, IOException  
     {
-      // step 1
-    	Document document = new Document();
-        // step 2
-    	PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("pdf.pdf"));
-        // step 3
-        document.open();
-        // step 4
-        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
-                new FileInputStream("index.html"));	
-        //step 5
-         document.close();
-
+    	
+    	// String to PDF
+    	HtmlConverter.convertToPdf(HTML, new FileOutputStream("string-to-pdf.pdf"));
+    	
+    	
+    	// HTML file to PDF
+    	HtmlConverter.convertToPdf(new FileInputStream("index.html"), 
+    			new FileOutputStream("index-to-pdf.pdf"));
+    	
+    	
         System.out.println( "PDF Created!" );
     }
 }
