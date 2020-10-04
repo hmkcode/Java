f9c521153fcee78a54735e782af2671e1cb1fd09
Sh1710 <72331454+Sh1710@users.noreply.github.com>
2020-10-04 16:03:09 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,13 @@
+# Java
+Java related code
+Simple addtion code
+class add
+{
+public static void main(String[]args);
+{
+  int a=3;
+  int b=4;
+  int sum=a+b;
+  System.out.pritln("your toal is" + sum);
+  }
+  }
